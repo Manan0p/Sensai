19c66822d6ef8d9e907ce884ebb1aaacd813a170
Manan <imananop@gmail.com>
2025-11-22 19:25:43 +0530

    hero scroll animation

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,17 +1,39 @@
+"use client";
+
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Button } from './ui/button'
 import Image from 'next/image'
 
 const HeroSection = () => {
+    const imageRef = useRef(null);
+
+    useEffect(() => {
+        const imageElement = imageRef.current;
+
+        const handleScroll =() => {
+            const scrollPosition = window.scrollY;
+            const scrollThreshold = 100;
+            
+            if (scrollPosition >scrollThreshold){
+                imageElement.classList.add("scrolled");
+            } else {
+                imageElement.classList.remove("scrolled");
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
   return <section className='w-full pt-36 md:pt-48 pb-10'>
     <div className='space-y-6 text-center'>
         <div className='space-y-6 mx-auto'>
-            <h1 className='gradient-title'>Your AI Career Coach for <br /> Professional Success</h1>
-            <p>Advance your career with personalized guidance, interview prep, and AI-powered tools for job success.</p>
+            <h1 className='gradient-title text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl'>Your AI Career Coach for <br /> Professional Success</h1>
+            <p className='mx-auto max-w-[600px] text-muted-foreground md:text-xl'>Advance your career with personalized guidance, interview prep, and AI-powered tools for job success.</p>
         </div>
 
-        <div>
+        <div className='flex justify-center space-x-4'>
             <Link href={"/dashboard"}>
                 <Button className={"px-8"} size={"lg"}>Get Started</Button>
             </Link>
@@ -21,8 +43,8 @@ const HeroSection = () => {
             </Link>
         </div>
 
-        <div>
-            <div>
+        <div className='hero-image-wrapper mt-5 md:mt-0'>
+            <div ref={imageRef} className='hero-image'>
                 <Image src={"/banner.jpeg"} width={1280} height={720} alt='Sensai Banner' className='rounded-lg shadow-2xl border mx-auto' priority/>
             </div>
         </div>
