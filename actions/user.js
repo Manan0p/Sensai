1c9e3175be9ef831f245fd813c917843d9c1b526
Manan <imananop@gmail.com>
2025-11-26 15:57:05 +0530

    user onboarding status

--- actions/user.js
+++ actions/user.js
@@ -69,5 +69,37 @@ export async function updateUser(data) {
 }
 
 export async function getUserOnboardingStatus() {
-    
+    const {userId} = await auth();
+    if (!userId) {
+        throw new Error("Unauthorized");
+    }
+
+    const user = await db.user.findUnique({
+        where: {
+            clerkUserId: userId,
+        },
+    });
+
+    if (!user){
+        throw new Error("User not found");
+    }
+
+    try {
+        const user = await db.user.findUnique({
+            where: {
+                clerkUserId: userId,
+            },
+            select: {
+                industry: true,
+            },
+        });
+
+        return {
+            isOnboarded: !!user?.industry,
+        };
+
+    } catch (error) {
+        console.error("Error checking onboarding status:", error.message);
+        throw new Error("Failed to check onboarding status");
+    }
 }
