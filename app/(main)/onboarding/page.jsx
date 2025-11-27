940b89747a11871e87e96ce130a08949e664a524
Manan <imananop@gmail.com>
2025-11-27 14:48:41 +0530

    onboarding redirect

--- app/(main)/onboarding/page.jsx
+++ app/(main)/onboarding/page.jsx
@@ -1,7 +1,14 @@
+import { getUserOnboardingStatus } from "@/actions/user"
 import { industries } from "@/data/industries"
+import { redirect } from "next/navigation";
+import OnboardingForm from "./_components/onboarding-form";
 
-const OnboardingPage = () => {
-  
+const OnboardingPage = async() => {
+  const {isOnboarded} =await getUserOnboardingStatus();
+
+  if(isOnboarded){
+    redirect('/dashboard');
+  }
 
   return (
     <main>
