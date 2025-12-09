3271f50abe1e3cfb6b4e68571710db73ba9d4091
Manan <imananop@gmail.com>
2025-12-09 23:50:02 +0530

    saving quiz result

--- actions/interview.js
+++ actions/interview.js
@@ -3,6 +3,7 @@
 import { db } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import { is } from "zod/v4/locales";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({
@@ -60,4 +61,80 @@ export async function generateQuiz() {
         console.error("Error generating quiz:", error);
         throw new Error("Failed to generate quiz");
     }
-};
+}
+
+
+export async function saveQuizResult(questions, answers, score) {
+    const {userId} = await auth();
+    if (!userId) {
+        throw new Error("Unauthorized");
+    }
+
+    const user = await db.user.findUnique({
+        where: {
+            clerkUserId: userId,
+        },
+    });
+
+    if (!user){
+        throw new Error("User not found");
+    }
+
+    const questionsResults = questions.map((q,index)=>({
+        question: q.question,
+        answer: q.correctAnswer,
+        userAnswer: answers[index],
+        isCorrect: q.correctAnswer === answers[index],
+        explaination: q.explanation,
+    }));
+
+    const wrongAnswers = questionsResults.filter((q) => !q.isCorrect);
+    let improvementTip = null;
+
+    if (wrongAnswers.length > 0){
+        const wrongQuestionsText = wrongAnswers
+            .map(
+                (q)=>
+                    `Question: "${q.question}"\nCorrect Answer: "${q.answer}"\nUser Answer: "${q.userAnswer}"`
+            )
+            .join("\n\n");
+
+        const improvementPrompt = `
+            The user got the following ${user.industry} technical interview questions wrong:
+
+            ${wrongQuestionsText}
+
+            Based on these mistakes, provide a concise, specific improvement tip.
+            Focus on the knowledge gaps revealed by these wrong answers.
+            Keep the response under 2 sentences and make it encouraging.
+            Don't explicitly mention the mistakes, instead focus on what to learn/practice.
+        `;
+
+        try {
+            const result = await model.generateContent(improvementPrompt);
+            const response = result.response;
+            improvementTip = response.text().trim();
+
+        } catch (error) {
+            console.error("Error generating improvement tip:", error);
+        }
+    }
+    
+    try {
+        const assessment = await db.assessment.create({
+            data: {
+                userId: user.id,
+                quizScore: score,
+                questions: questionsResults,
+                category: "technical",
+                improvementTip
+            },
+        });
+
+        return assessment;
+
+    } catch (error) {
+        console.error("Error saving quiz result:", error);
+        throw new Error("Failed to save quiz result");
+    }
+}
