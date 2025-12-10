a7846794b73457c3ff334e463b1f7b2b561ad0f6
Manan <imananop@gmail.com>
2025-12-11 02:38:59 +0530

    mock interview ui

--- app/(main)/interview/_components/quiz.jsx
+++ app/(main)/interview/_components/quiz.jsx
@@ -1,4 +1,22 @@
+"use client";
+
+import { generateQuiz } from "@/actions/interview";
+import useFetch from "@/hooks/use-fetch";
+import { useState } from "react";
+
 const Quiz = () => {
+
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [answers, setAnswers] = useState({});
+  const [showExplanations, setShowExplanations] = useState(false);
+  
+  const {
+    loading: generatingQuiz,
+    fn: generateQuizFn,
+    data: quizData,
+  } = useFetch(generateQuiz);
+
+
   return (
     <div>Quiz</div>
   )
