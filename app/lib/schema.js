a2a759ca34ed41f2e34c60dcc99581584324017a
Manan <imananop@gmail.com>
2025-12-13 04:31:41 +0530

    resume schema

--- app/lib/schema.js
+++ app/lib/schema.js
@@ -54,3 +54,12 @@ export const entrySchema = z.object({
         path: ["endDate"],
     }
 );
+
+export const resumeSchema = z.object({
+    contactInfo: contactSchema,
+    summary: z.string().min(1, "Professional summary is required"),
+    skills: z.string().min(1, "Skills are required"),
+    experince: z.array(entrySchema),
+    education: z.array(entrySchema),
+    projects: z.array(entrySchema),
+});
